827502aac605365f9de7c62beb6ea413a4977ad3
Gustavo Franco <gustavocfranco@gmail.com>
2013-06-03 10:48:28 -0300

    All tabs were replaced by 4 spaces. Sorry, my bad :(

--- dragons.js
+++ dragons.js
@@ -1,45 +1,45 @@
 var CodeMaintenance = (function() {
-	'use strict';
+    'use strict';
 
-	CodeMaintenance = function() {
-		this.warriors = [];
-		this.dragons = [];
-	};
+    CodeMaintenance = function() {
+        this.warriors = [];
+        this.dragons = [];
+    };
 
-	CodeMaintenance.prototype.addWarrior = function(warrior) {
-		this.warriors.push(warrior);
+    CodeMaintenance.prototype.addWarrior = function(warrior) {
+        this.warriors.push(warrior);
 
-		return this;
-	};
+        return this;
+    };
 
-	CodeMaintenance.prototype.addDragon = function(dragon) {
-		this.dragons.push(dragon);
+    CodeMaintenance.prototype.addDragon = function(dragon) {
+        this.dragons.push(dragon);
 
-		return this;
-	};
+        return this;
+    };
 
-	CodeMaintenance.prototype.fight = function(rounds) {
-		try {
-			if ((this.warriors.length > 0) && (this.dragons.length > 0)) {
-				for (var index = 0; index < rounds; index++) {
-					var warrior = this.warriors[Math.floor(Math.random() * this.warriors.length)];
-					var dragon = this.dragons[Math.floor(Math.random() * this.dragons.length)];
+    CodeMaintenance.prototype.fight = function(rounds) {
+        try {
+            if ((this.warriors.length > 0) && (this.dragons.length > 0)) {
+                for (var index = 0; index < rounds; index++) {
+                    var warrior = this.warriors[Math.floor(Math.random() * this.warriors.length)];
+                    var dragon = this.dragons[Math.floor(Math.random() * this.dragons.length)];
 
-					if (!!Math.round(Math.random() * 1)) {
-						console.log(warrior + ' fought ' + dragon + ' and won!');
-					} else {
-						console.log(warrior + ' fought ' + dragon + ' and lost.');
-					}
-				}
-			} else {
-				throw('You must supply, at least, one warrior and one dragon');
-			}
-		} catch (exception) {
-			console.log(exception);
-		}
-	};
+                    if (!!Math.round(Math.random() * 1)) {
+                        console.log(warrior + ' fought ' + dragon + ' and won!');
+                    } else {
+                        console.log(warrior + ' fought ' + dragon + ' and lost.');
+                    }
+                }
+            } else {
+                throw('You must supply, at least, one warrior and one dragon');
+            }
+        } catch (exception) {
+            console.log(exception);
+        }
+    };
 
-	return CodeMaintenance;
+    return CodeMaintenance;
 })();
 
 // runner
