462a8d987ddf4c12efc04aa6adf7d6990c234043
unknown <gustavo.franco@eMiolo-03-PC.BIOKRATOS.local>
2013-06-03 10:33:59 -0300

    All double quotes was removed

--- dragons.js
+++ dragons.js
@@ -26,13 +26,13 @@ var CodeMaintenance = (function() {
 					var dragon = this.dragons[Math.floor(Math.random() * this.dragons.length)];
 
 					if (!!Math.round(Math.random() * 1)) {
-						console.log(warrior + " fought " + dragon + " and won!");
+						console.log(warrior + ' fought ' + dragon + ' and won!');
 					} else {
-						console.log(warrior + " fought " + dragon + " and lost.");
+						console.log(warrior + ' fought ' + dragon + ' and lost.');
 					}
 				}
 			} else {
-				throw("You must supply, at least, one warrior and one dragon");
+				throw('You must supply, at least, one warrior and one dragon');
 			}
 		} catch (exception) {
 			console.log(exception);
@@ -44,44 +44,44 @@ var CodeMaintenance = (function() {
 
 // runner
 var codeMaintenance = new CodeMaintenance();
-codeMaintenance.addWarrior("Jorge Roberto")
-               .addWarrior("Sillas Soares")
-               .addWarrior("Sotnas Leunam")
-               .addWarrior("Fabio Porto")
-               .addWarrior("Diego Oliveira")
-               .addWarrior("Gustavo Franco")
-               .addWarrior("André Augusto")
-               .addWarrior("Tadeu Zagallo")
-               .addDragon("Devon & Cornwall")
-               .addDragon("Elliot")
-               .addDragon("Gorbash")
-               .addDragon("Mushu")
-               .addDragon("Smrgol")
-               .addDragon("Vermithrax Pejorative")
-               .addDragon("Yowler")
-               .addDragon("Chrysophylax")
-               .addDragon("Custard")
-               .addDragon("Glaurung")
-               .addDragon("Griaule")
-               .addDragon("Icefyre")
-               .addDragon("Norbert")
-               .addDragon("Pern Dragon")
-               .addDragon("Prothero")
-               .addDragon("Romanian Longhorn")
-               .addDragon("Saphira")
-               .addDragon("Scatha")
-               .addDragon("Smaug")
-               .addDragon("Tintaglia")
-               .addDragon("Balerion")
-               .addDragon("Meraxes")
-               .addDragon("Vhagar")
-               .addDragon("Silverwing")
-               .addDragon("Vermithrax")
-               .addDragon("Ghiscar")
-               .addDragon("Valrion")
-               .addDragon("Esovius")
-               .addDragon("Archonae")
-               .addDragon("Drogon")
-               .addDragon("Rhaegal")
-               .addDragon("Viserion")
+codeMaintenance.addWarrior('Jorge Roberto')
+               .addWarrior('Sillas Soares')
+               .addWarrior('Sotnas Leunam')
+               .addWarrior('Fabio Porto')
+               .addWarrior('Diego Oliveira')
+               .addWarrior('Gustavo Franco')
+               .addWarrior('André Augusto')
+               .addWarrior('Tadeu Zagallo')
+               .addDragon('Devon & Cornwall')
+               .addDragon('Elliot')
+               .addDragon('Gorbash')
+               .addDragon('Mushu')
+               .addDragon('Smrgol')
+               .addDragon('Vermithrax Pejorative')
+               .addDragon('Yowler')
+               .addDragon('Chrysophylax')
+               .addDragon('Custard')
+               .addDragon('Glaurung')
+               .addDragon('Griaule')
+               .addDragon('Icefyre')
+               .addDragon('Norbert')
+               .addDragon('Pern Dragon')
+               .addDragon('Prothero')
+               .addDragon('Romanian Longhorn')
+               .addDragon('Saphira')
+               .addDragon('Scatha')
+               .addDragon('Smaug')
+               .addDragon('Tintaglia')
+               .addDragon('Balerion')
+               .addDragon('Meraxes')
+               .addDragon('Vhagar')
+               .addDragon('Silverwing')
+               .addDragon('Vermithrax')
+               .addDragon('Ghiscar')
+               .addDragon('Valrion')
+               .addDragon('Esovius')
+               .addDragon('Archonae')
+               .addDragon('Drogon')
+               .addDragon('Rhaegal')
+               .addDragon('Viserion')
                .fight(50);
