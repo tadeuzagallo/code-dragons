34cf5c55fb73a2b58f5231b5f8f57799fd18214d
tadeuzagallo <tadeuzagallo@gmail.com>
2013-06-04 22:11:39 -0300

    Dragons and Warriors now are Classes and have Levels

--- dragons.js
+++ dragons.js
@@ -2,7 +2,7 @@
 
 var CodeMaintenance = (function() {
     'use strict';
-    
+
     /**
      * Constructor method
      *
@@ -15,6 +15,18 @@ var CodeMaintenance = (function() {
         this.expectators = [];
     };
 
+    CodeMaintenance.Warrior = function() {
+      this.name = '';
+      this.level = 1;
+      this.dragons = [];
+    };
+
+    CodeMaintenance.Dragon = function() {
+      this.name = '';
+      this.owner = undefined;
+      this.level = 1;
+    };
+
     /**
      * Add warrior
      *
@@ -22,10 +34,21 @@ var CodeMaintenance = (function() {
      * @author Gustavo Franco
      * @since  2013-06-03
      */
-    CodeMaintenance.prototype.addWarrior = function(warrior) {
+    CodeMaintenance.prototype.addWarrior = function(name, level) {
+        if (typeof(name) !== 'string' || !name) {
+          throw new TypeError('Warrior name "' + name + '" is invalid');
+        }
+
+        var warrior = new CodeMaintenance.Warrior();
+        warrior.name = name;
+
+        if (level !== undefined) {
+          warrior.level = level;
+        }
+
         this.warriors.push(warrior);
 
-        return this;
+        return warrior;
     };
 
     /**
@@ -35,10 +58,24 @@ var CodeMaintenance = (function() {
      * @author Gustavo Franco
      * @since  2013-06-03
      */
-    CodeMaintenance.prototype.addDragon = function(dragon) {
+    CodeMaintenance.prototype.addDragon = function(owner, name, level) {
+        if (!(owner instanceof CodeMaintenance.Warrior)) {
+          throw new TypeError('Dragon owner must be an CodeMaintenance.Warrior instance create by CodeMaintenance.addWarrior()');
+        } else if (typeof(name) !== 'string' || !name) {
+          throw new TypeError('Dragon name "' + name + '" is invalid');
+        }
+
+        var dragon = new CodeMaintenance.Dragon();
+        dragon.owner = owner;
+        dragon.name = name;
+
+        if (level !== undefined) {
+          dragon.level = level;
+        }
+
         this.dragons.push(dragon);
 
-        return this;
+        return dragon;
     };
 
     /**
@@ -63,6 +100,14 @@ var CodeMaintenance = (function() {
      * @since  2013-06-03
      */
     CodeMaintenance.prototype.fight = function(rounds) {
+        var won = function(warrior, dragon) {
+          return result('won', warrior, dragon);
+        }, lost = function(warrior, dragon) {
+          return result('lost', warrior, dragon);
+        }, result = function(result, warrior, dragon) {
+          return console.log(warrior.name + '(' + warrior.level + ') fought ' + dragon.name + '(' + dragon.level + ') and ' + result + '!');
+        };
+
         try {
             if (this.warriors.length && this.dragons.length && this.expectators.length) {
                 for (var index = 0; index < rounds; index++) {
@@ -70,26 +115,26 @@ var CodeMaintenance = (function() {
                     var dragon = this.dragons[Math.floor(Math.random() * this.dragons.length)];
                     var expectator = this.expectators[Math.floor(Math.random() * this.expectators.length)];
 
-                    if (!!Math.round(Math.random() * 1)) {
-                        console.log(warrior + ' fought ' + dragon + ' and won!');
+                    if (Math.random() * warrior.level > Math.random() * dragon.level) {
+                        won(warrior, dragon);
                         
                         if (!!Math.round(Math.random() * 1)) {
-                            console.log(expectator + ' applauded!');
+                            console.log('   ' + expectator + ' applauded!');
                         } else {
-                            console.log(expectator + ' thinks ' + warrior + ' cheated. Bad fight.');
+                            console.log('   ' + expectator + ' thinks ' + warrior.name + ' cheated. Bad fight.');
                         }
                     } else {
-                        console.log(warrior + ' fought ' + dragon + ' and lost.');
+                        lost(warrior, dragon);
                         
                         if (!!Math.round(Math.random() * 1)) {
-                            console.log(expectator + ' took the burnt remains home for a barbecue. :)');
+                            console.log('   ' + expectator + ' took the burnt remains home for a barbecue. :)');
                         } else {
-                            console.log(expectator + ' mourned the passing of ' + warrior);
+                            console.log('   ' + expectator + ' mourned the passing of ' + warrior.name);
                         }
                     }
                 }
             } else {
-                throw('You must supply, at least, one warrior, dragon and expectator.');
+                throw new Error('You must supply, at least, one warrior, dragon and expectator.');
             }
         } catch (exception) {
             console.log(exception);
@@ -100,46 +145,63 @@ var CodeMaintenance = (function() {
 })();
 
 // runner
-var codeMaintenance = new CodeMaintenance();
-codeMaintenance.addWarrior('Jorge Roberto')
-               .addWarrior('Sillas Soares')
-               .addWarrior('Sotnas Leunam')
-               .addWarrior('Fabio Porto')
-               .addWarrior('Diego Oliveira')
-               .addWarrior('Gustavo Franco')
-               .addWarrior('André Augusto')
-               .addWarrior('Tadeu Zagallo')
-               .addDragon('Devon & Cornwall')
-               .addDragon('Elliot')
-               .addDragon('Gorbash')
-               .addDragon('Mushu')
-               .addDragon('Smrgol')
-               .addDragon('Vermithrax Pejorative')
-               .addDragon('Yowler')
-               .addDragon('Chrysophylax')
-               .addDragon('Custard')
-               .addDragon('Glaurung')
-               .addDragon('Griaule')
-               .addDragon('Icefyre')
-               .addDragon('Norbert')
-               .addDragon('Pern Dragon')
-               .addDragon('Prothero')
-               .addDragon('Romanian Longhorn')
-               .addDragon('Saphira')
-               .addDragon('Scatha')
-               .addDragon('Smaug')
-               .addDragon('Tintaglia')
-               .addDragon('Balerion')
-               .addDragon('Meraxes')
-               .addDragon('Vhagar')
-               .addDragon('Silverwing')
-               .addDragon('Vermithrax')
-               .addDragon('Ghiscar')
-               .addDragon('Valrion')
-               .addDragon('Esovius')
-               .addDragon('Archonae')
-               .addDragon('Drogon')
-               .addDragon('Rhaegal')
-               .addDragon('Viserion')
-               .addExpectator('Patola')
-               .fight(50);
+var codeMaintenance = new CodeMaintenance(),warriorNames = [
+  'Jorge Roberto',
+  'Sillas Soares',
+  'Sotnas Leunam',
+  'Fabio Porto',
+  'Diego Oliveira', 
+  'Gustavo Franco', 
+  'André Augusto',
+  'Tadeu Zagallo'
+], dragonNames = [
+  'Devon & Cornwall',
+  'Elliot',
+  'Gorbash',
+  'Mushu',
+  'Smrgol',
+  'Vermithrax Pejorative',
+  'Yowler',
+  'Chrysophylax',
+  'Custard',
+  'Glaurung',
+  'Griaule',
+  'Icefyre',
+  'Norbert',
+  'Pern Dragon',
+  'Prothero',
+  'Romanian Longhorn',
+  'Saphira',
+  'Scatha',
+  'Smaug',
+  'Tintaglia',
+  'Balerion',
+  'Meraxes',
+  'Vhagar',
+  'Silverwing',
+  'Vermithrax',
+  'Ghiscar',
+  'Valrion',
+  'Esovius',
+  'Archonae',
+  'Drogon',
+  'Rhaegal',
+  'Viserion',
+], i, randomLevel = function() {
+  return Math.floor(Math.random() * 100) + 1;
+}, randomWarrior = function() {
+  return codeMaintenance.warriors[Math.floor(Math.random() * codeMaintenance.warriors.length)];
+};
+
+
+for (i = 0; i < warriorNames.length; i++) {
+  codeMaintenance.addWarrior(warriorNames[i], randomLevel());
+}
+
+for (i = 0; i < dragonNames.length; i++) {
+  codeMaintenance.addDragon(randomWarrior(), dragonNames[i], randomLevel());
+}
+
+codeMaintenance.addExpectator('Patola');
+
+codeMaintenance.fight(50);
